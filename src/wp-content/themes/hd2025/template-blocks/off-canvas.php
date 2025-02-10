ee43f2de8622c0207d23cb24beda73fe6eedb76c
Ngô Dũng <dungnq81@gmail.com>
2025-02-10 10:50:34 +0700

    update

--- src/wp-content/themes/hd2025/template-blocks/off-canvas.php
+++ src/wp-content/themes/hd2025/template-blocks/off-canvas.php
@@ -6,21 +6,19 @@
  * @author Gaudev
  */
 
-use HD\Helper;
-
 \defined( 'ABSPATH' ) || die;
 
-$txt_logo = Helper::getOption( 'blogname' );
-$img_logo = Helper::getThemeMod( 'alt_logo' );
+$txt_logo = \HD\Helper::getOption( 'blogname' );
+$img_logo = \HD\Helper::getThemeMod( 'alt_logo' );
 
 if ( ! $img_logo ) :
-	$html = sprintf( '<a href="%1$s" class="mobile-logo-link" rel="home" aria-label="%2$s">%3$s</a>', Helper::home(), Helper::escAttr( $txt_logo ), $txt_logo );
+	$html = sprintf( '<a href="%1$s" class="mobile-logo-link" rel="home" aria-label="%2$s">%3$s</a>', \HD\Helper::home(), \HD\Helper::escAttr( $txt_logo ), $txt_logo );
 else :
 	$image = '<img src="' . $img_logo . '" alt="logo">';
-	$html  = sprintf( '<a href="%1$s" class="mobile-logo-link" rel="home">%2$s</a>', Helper::home(), $image );
+	$html  = sprintf( '<a href="%1$s" class="mobile-logo-link" rel="home">%2$s</a>', \HD\Helper::home(), $image );
 endif;
 
-$position = Helper::getThemeMod( 'offcanvas_menu_setting' );
+$position = \HD\Helper::getThemeMod( 'offcanvas_menu_setting' );
 if ( ! in_array( $position, [ 'left', 'right', 'top', 'bottom' ], false ) ) {
 	$position = 'right';
 }
@@ -37,8 +35,8 @@ if ( ! in_array( $position, [ 'left', 'right', 'top', 'bottom' ], false ) ) {
     <div class="menu-outer">
 		<?php
 
-		echo Helper::doShortcode( 'inline_search' );
-		echo Helper::doShortcode( 'vertical_menu' );
+		echo \HD\Helper::doShortcode( 'inline_search' );
+		echo \HD\Helper::doShortcode( 'vertical_menu' );
 		?>
     </div>
 </div>
