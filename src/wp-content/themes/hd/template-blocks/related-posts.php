16c004f66cb192718bf8be8b9d8fbb18bda65912
Ngô Dũng <dungnq81@gmail.com>
2025-03-14 17:30:52 +0700

    update

---
+++ src/wp-content/themes/hd/template-blocks/related-posts.php
@@ -0,0 +1,23 @@
+<?php
+
+\defined( 'ABSPATH' ) || die;
+
+$post_id = $args['post_id'] ?? 0;
+$term    = $args['term'] ?? 'category';
+$title   = $args['title'] ?? '';
+
+$num_max = 12;
+$posts = \HD\Helper::getRelatedPosts( $post_id, $term, $num_max );
+if ( ! $posts ) {
+	return;
+}
+
+?>
+<section class="section section-related archive section-related-post">
+	<div class="container">
+		<div class="items">
+			<?php echo $title ? '<p class="related-title">' . $title . '</p>' : ''; ?>
+
+		</div>
+	</div>
+</section>
