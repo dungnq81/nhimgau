dee61d503d05d92c44523e37ee276852432b6009
Ngô Dũng <dungnq81@gmail.com>
2025-03-24 17:09:36 +0700

    update

--- src/wp-content/themes/hd/comments.php
+++ src/wp-content/themes/hd/comments.php
@@ -15,7 +15,66 @@ if ( post_password_required() ) {
 	return;
 }
 
-$comment_count = get_comments_number();
+if ( ! post_type_supports( get_post_type(), 'comments' ) ) {
+	return;
+}
+
+if ( ! have_comments() && ! comments_open() ) {
+	return;
+}
 
 ?>
-<div id="comments" class="comments-area wp-comments-area"></div>
+<section id="comments" class="comments-area">
+
+	<?php if ( have_comments() ) : ?>
+        <h2 class="title-comments">
+			<?php
+			$comments_number = get_comments_number();
+			if ( '1' === $comments_number ) {
+				printf( esc_html_x( 'One Response', 'comments title', TEXT_DOMAIN ) );
+			} else {
+				printf(
+				    /* translators: %s: Number of comments. */
+					esc_html(
+						_nx(
+							'%s Response',
+							'%s Responses',
+							$comments_number,
+							'comments title',
+							TEXT_DOMAIN
+						)
+					),
+					esc_html( number_format_i18n( $comments_number ) )
+				);
+			}
+			?>
+        </h2>
+
+		<?php the_comments_navigation(); ?>
+
+        <ol class="comment-list">
+			<?php
+			wp_list_comments(
+				[
+					'style'       => 'ol',
+					'short_ping'  => true,
+					'avatar_size' => 42,
+				]
+			);
+			?>
+        </ol>
+
+		<?php the_comments_navigation(); ?>
+
+	<?php endif; ?>
+
+	<?php
+	comment_form(
+		[
+			'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+			'title_reply_after'  => '</h2>',
+		]
+	);
+	?>
+
+</section>
