7d5b4c2bbea7b95373f638e4745aa48120854c35
Ngô Dũng <dungnq81@gmail.com>
2025-04-06 00:07:02 +0700

    update

--- src/wp-content/themes/hd/template-blocks/category-tabs.php
+++ src/wp-content/themes/hd/template-blocks/category-tabs.php
@@ -2,4 +2,9 @@
 
 \defined( 'ABSPATH' ) || die;
 
+$title     = $args['title'] ?? '';
+$title_tag = $args['title_tag'] ?? 'p';
 $term_ids  = $args['term_ids'] ?? [];
+$max       = $args['max'] ?? 6;
+$post_type = $args['post_type'] ?? 'post';
+$taxonomy  = $args['taxonomy'] ?? 'category';
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2273e620c1e409a6b2f84fb50ba5d867af2fbde4',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2273e620c1e409a6b2f84fb50ba5d867af2fbde4',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
